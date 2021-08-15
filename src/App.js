83c675f83cb572170704b96968913cd1098a5224
Daniel Gamboa <d.2610@hotmail.com>
2021-08-15 18:40:27 -0500

    Se agrega HomeContainer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { NavBarComponent } from './components/NavBar';
+import { HomeContainer } from './containers/HomeContainer';
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <NavBarComponent/>
+        <NavBarComponent/>
+        <Switch>
+            <Route exact path={'/'}>
+              <HomeContainer greeting={'¡Conoce nuestras categorias!'} />
+            </Route>
+        </Switch>
       </BrowserRouter>
     </>
   );
---
+++ src/containers/HomeContainer/index.jsx
@@ -0,0 +1,40 @@
+// import { useEffect, useState } from "react";
+// import { CategoriesComponent } from "../../components/CategoriesComponent";
+// import { getFirestore } from "../../firebase";
+import { Row, Container, Spinner } from 'react-bootstrap';
+
+export function HomeContainer({ greeting }) {
+//   const [categories, setCategories] = useState();
+
+//   useEffect(() => {
+//     // PARA OBTENER LA COLECCION DE CATEGORIAS
+//     async function getDataFromFirestore() {
+//       const BD = getFirestore();
+//       const collection = BD.collection('categorias').orderBy('name', 'asc');
+//       const response = await collection.get();
+//       setCategories(response.docs.map(element => ({ ...element.data() })));
+//     }
+//     getDataFromFirestore();
+
+//   }, [])
+
+  return (
+    <>
+      <Container>
+        <Row>
+          {/* <h4 className="text-center mt-3">{greeting}</h4>
+          {categories ?
+            categories.map((element) => {
+              return (
+                <CategoriesComponent key={element.name} name={element.name} img={element.img}  />
+              )
+            })
+            :
+            <Spinner animation="border" variant="info" />} */}
+            <h4 className="text-center mt-3">{greeting}</h4>
+            
+        </Row>
+      </Container>
+    </>
+  )
+};
